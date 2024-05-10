be672054054b4269d1ff410ee69e7da98f9e7f99
Jay <wmeng0103@gmail.com>
2024-05-11 00:28:19 +0900

    update

--- src/app/(home)/cart/page.tsx
+++ src/app/(home)/cart/page.tsx
@@ -5,13 +5,18 @@ import { useAppSelector, useAppDispatch } from '@/store/hooks';
 import { CartProduct } from "@/components/ProductCardItem";
 import { IProduct } from '@/store/features/products/productsAPI';
 import { useRouter } from 'next/navigation';
+import { selectUser } from "@/store/features/auth/authSlice";
 import { selectProducts, getProducts } from '@/store/features/products/productsSlice';
 import { Button } from "@/components/ui/button";
 
 export default function About() {
+  
+  const dispatch = useAppDispatch();
+  const user = useAppSelector(selectUser);
   const products: IProduct[] = useAppSelector(selectProducts);
   const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cartItems') || '[]'));
   const subtotal = cartItems.reduce((acc, item) => acc + (item.qty * Number(products.find(p => p.id === item.product_id)?.price || 0)), 0);
+  dispatch(getProducts({ type: "all", user: user.id }));
   const router = useRouter();
   return (
     <main className=" min-h-screen mb-20 pt-10 pb-10 px-5 md:px-20">
@@ -31,19 +36,21 @@ export default function About() {
         </div>
         {cartItems.map((item: CartProduct, index: number) => {
           const product : IProduct = products.find(p => p.id === item.product_id);
+          console.log(products)
+          console.log(product)
           return (
             <div key={index} className="w-full max-w-[1280px] flex border-b-[1px] border-gray-300 pb-3 mx-auto mt-6">
               <div className="w-full text-left sm:flex">
                 <Image
                   className='w-[70px] bg-white rounded-full content-center aspect-square object-cover'
-                  src={product.image}
+                  src={product?.image}
                   alt='grass'
                   width={300}
                   height={300}
                 />
                 <div className="w-full content-center">
                   <p className="sm:ml-6 w-full text-left content-center">
-                    {product.title}
+                    {product?.title}
                   </p>  
                   <p className="sm:ml-6 w-full text-left content-center">
                     <span className="font-bold">Flavor</span> : {item.flavor_name}
@@ -66,7 +73,7 @@ export default function About() {
               <div className="w-full sm:flex content-center">
                 <p className="w-full text-center sm:text-left content-center">$ {product? product.price : 'N/A'}</p>
                 <p className="w-full text-center sm:text-left content-center">{item.qty}</p>
-                <p className="w-full text-center sm:text-right content-center">$ {item.qty * Number(product.price)}</p>
+                <p className="w-full text-center sm:text-right content-center">$ {item.qty * Number(product?.price)}</p>
               </div>
             </div>
           );
